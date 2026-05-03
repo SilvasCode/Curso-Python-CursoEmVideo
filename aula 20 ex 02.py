1c4e9ab6f92108ab1b1d93cc05d5252b24e1158d
Andrey Almeida Da Silva <andreyalmeida076@gmail.com>
2026-05-03 11:22:47 +0100

    Aula 20 - ex 02

---
+++ aula 20 ex 02.py
@@ -0,0 +1,12 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('=' * tam)
+    print(msg.center(tam))
+    print('=' * tam)
+
+
+escreva('Curso Python')
+
+escreva('Andrey Almeida Da Silva')
+
+escreva('def')
