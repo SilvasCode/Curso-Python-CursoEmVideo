7c02f88af36e0fce33b75ad12506af2e2b9fa12f
Andrey Almeida Da Silva <andreyalmeida076@gmail.com>
2026-05-17 15:14:17 +0100

    Aula 21 - ex 02

---
+++ aula 21 ex 02.py
@@ -0,0 +1,25 @@
+def fatorial(num, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param num: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta na tela.
+    :return: O valor do Fatorial de um número n.
+    """
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+    return f
+
+
+# [--- Programa Principal ---]
+print('=-=' * 10)
+print(f'O fatorial de 5 é {fatorial(5)}')
+
+print(fatorial(5, show=True))
+print('=-=' * 10)
