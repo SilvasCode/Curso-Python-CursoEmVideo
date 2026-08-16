1988e3fea7f0ca5b2088b524f05198426cfec05b
Andrey Almeida Da Silva <andreyalmeida076@gmail.com>
2026-08-16 15:20:52 +0100

    Aula 4 - POO ex 03

---
+++ exercicios POO/POO - ex 03/ex 03.py
@@ -0,0 +1,36 @@
+class ContaBancaria:
+    """
+Cria uma conta bancária e permite fazer saques e depósitos
+    """
+    def __init__(self, id, nome, saldo = 0): # met construtor
+        self.id = id
+        self.titular = nome
+        self.saldo = saldo
+        print(f"[Conta: {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}]")
+
+    def __str__(self):
+        print("-" * 47)
+        return f"A conta: {self.id} de {self.titular} tem R${self.saldo:,.2f} de saldo"
+
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print("-" * 47)
+        print(f"+ Depósito de R${valor:,.2f} autorizado na conta: {self.id}")
+
+
+    def sacar(self, valor):
+        if valor > self.saldo:
+            print(f"X Saque NEGADO de R${valor:,.2f} na conta {self.id}: [SALDO INSUFICIENTE]")
+        else:
+            self.saldo -= valor
+            print("-" * 47)
+            print(f"- Saque de R${valor:,.2f} autorizado na conta: {self.id}")
+
+
+
+c1 = ContaBancaria(112, "Andrey", 3000)
+c1.depositar(500)
+c1.sacar(10000)
+print(c1)
+
